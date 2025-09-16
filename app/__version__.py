6d53b8161d5137949783b9513412a1053ea1cc7f
Shannon Atkinson <rainmanjam@gmail.com>
2025-09-15 17:44:15 -0700

    Update __version__.py

--- app/__version__.py
+++ app/__version__.py
@@ -6,7 +6,7 @@ It is used by the Makefile for version management and by the application
 to display version information.
 """
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 __author__ = "Social Flood Team"
 __email__ = "info@socialflood.com"
 __description__ = "API for social media data aggregation and analysis"
