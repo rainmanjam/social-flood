413f2a19ab4679c7b13ceda70c2f3517946621d3
GitHub Action <action@github.com>
2025-09-16 00:28:13 +0000

    chore: bump version to v1.3.2

--- app/__version__.py
+++ app/__version__.py
@@ -6,7 +6,7 @@ It is used by the Makefile for version management and by the application
 to display version information.
 """
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 __author__ = "Social Flood Team"
 __email__ = "info@socialflood.com"
 __description__ = "API for social media data aggregation and analysis"
